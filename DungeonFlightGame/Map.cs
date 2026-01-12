d25fb4dd396c783837e995e61185808bd7848f2b
Maximme <112176330+Maximillian360@users.noreply.github.com>
2026-01-12 14:28:55 +0800

    Refactored Map methods to be one-liners.

--- DungeonFlightGame/Map.cs
+++ DungeonFlightGame/Map.cs
@@ -19,15 +19,9 @@ public class Map
 
     public Entity? GetEntity(int x, int y) => IsPositionInside(x, y) ? WorldMap[x, y].Entity : null;
 
-    public bool IsTileOccupied(int x, int y)
-    {
-        return GetEntity(x, y) != null;
-    }
+    public bool IsTileOccupied(int x, int y) => GetEntity(x, y) != null;
     
-    public bool IsPositionInside(int newX, int newY)
-    {
-        return newX >= 0 && newY >= 0 && newX < WorldMapCols && newY < WorldMapRows;
-    }
+    public bool IsPositionInside(int newX, int newY) => newX >= 0 && newY >= 0 && newX < WorldMapRows && newY < WorldMapCols;
 
     public bool TryPlaceEntity(Entity entity, (int x, int y) entityPosition)
     {
@@ -49,8 +43,16 @@ public class Map
             Console.WriteLine($"Position: {newPosition.x}, {newPosition.y} is occupied!");
             return;
         }
+
+        if (WorldMap[newPosition.x, newPosition.y].Entity != entity)
+        {
+            Console.WriteLine($"Entity at position: {newPosition.x}, {newPosition.y} is not equal to {entity.PositionX}, {entity.PositionY}!");
+            return;
+        }
+        
         WorldMap[newPosition.x, newPosition.y].Entity = WorldMap[currentX, currentY].Entity;
         WorldMap[currentX, currentY].Entity = null;
+        
     }
 
     public void MapMaker()
@@ -75,11 +77,13 @@ public class Map
                 if (i == 0 && j == 0)
                 {
                     Entity player = Entity.PlayerFactory(); 
+                    WorldMap[i, j].Entity = player;
                 }
 
                 else
                 {
                     Entity enemy = Entity.EnemyFactory(i, j);
+                    WorldMap[i, j].Entity = enemy;
                 }
 
             }
