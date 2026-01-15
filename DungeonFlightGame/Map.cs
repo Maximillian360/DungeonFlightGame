504b592836e5bc4790a369d58e3a7591fa8c99b7
Maximme <112176330+Maximillian360@users.noreply.github.com>
2026-01-16 00:42:58 +0800

    Added GetTile method in Map class to check if a coordinate in the WorldMap[,] has a Tile or not.

--- DungeonFlightGame/Map.cs
+++ DungeonFlightGame/Map.cs
@@ -18,6 +18,8 @@ public class Map
 
     public Entity? GetEntity(int x, int y) => IsPositionInside(x, y) ? WorldMap[x, y].Entity : null;
 
+    public Tile? GetTile(int x, int y) => IsPositionInside(x, y) ? WorldMap[x, y] : null;
+
     public bool IsTileOccupied(int x, int y) => GetEntity(x, y) != null;
     
     public bool IsPositionInside(int newX, int newY) => newX >= 0 && newY >= 0 && newX < WorldMapRows && newY < WorldMapCols;
@@ -28,6 +30,12 @@ public class Map
         if (!IsPositionInside(x, y))
         {
             Console.WriteLine($"Error, Position: {x}, {y} is out of bounds!");
+            return false;
+        }
+
+        if (GetTile(x, y) == null)
+        {
+            Console.WriteLine($"Error, Position: {x}, {y} has no Tile!");
             return false;
         }
 
